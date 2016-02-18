8d9fd499900399c439472582f0537f1e796c822d
ElizabethKMathew <elizabeth@qburst.com>
2016-02-18 19:04:57 +0530

    oop concepts example

---
+++ inheritance/proto.js
@@ -0,0 +1,21 @@
+            var myMammal = {
+            name : 'Herb the Mammal',
+            get_name : function ( ) {
+            return this.name;
+            }
+            };
+            var name = myMammal.get_name( );
+            console.log(name);
+            var myCat = Object.create(myMammal);
+            myCat.name = 'Kitty';
+            myCat.saying = 'meow';
+            myCat.says = function ( ) {
+            return this.saying || '';
+            };
+            myCat.get_name = function ( ) {
+            return this.says( ) + ' ' + this.name + ' ' + this.says( );
+            };
+            var says = myCat.says( );
+            var name = myCat.get_name( );
+            console.log(says);
+            console.log(name);
