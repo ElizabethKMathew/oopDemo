67d76bb9b3def52f1ac85a7a0a60357d3c3c6140
ElizabethKMathew <elizabeth@qburst.com>
2016-02-19 12:55:13 +0530

    Adding pages

---
+++ info-hiding/data-hiding.js
@@ -0,0 +1,35 @@
+var User = function(options) {
+        var name = options.name;
+        var password = options.password;
+        var birthYear = options.birthYear;
+        var age;
+        this.country = options.country;
+       
+
+        function privateMethod() {
+            console.log('im a private method!')
+        }
+        var calculateAge = function() {
+                var today = new Date();
+                age = today.getFullYear() - birthYear;
+                return age;
+            }
+
+            //Privileged method
+        this.getAge = function() {
+            return calculateAge(); // private variable
+        }
+    }
+    //new instance of User
+var user1 = new User({
+    name: 'Anju',
+    password: '***',
+    birthYear: 1993,
+    country: 'India'
+});
+
+console.log(u.name); //undefined
+console.log(u.country); //India
+console.log(u.age); //undefined
+console.log(u.getAge());
+console.log(u.privateMethod ()); //throws error
---
+++ inheritance/pseudo.js
@@ -0,0 +1,27 @@
+            var Mammal = function (name) {
+            this.name = name;
+            };
+            Mammal.prototype.get_name = function ( ) {
+            return this.name;
+            };
+            var myMammal = new Mammal('Feline');
+            var name = myMammal.get_name( );
+            console.log(name);
+            var Cat = function (name) {
+            this.name = name;
+            this.saying = 'meow';
+            };
+            Cat.prototype = new Mammal( );
+            Cat.prototype.get_name = function ( ) {
+            return this.says( ) + ' ' + this.name +
+            ' ' + this.says( );
+            };
+            Cat.prototype.says = function ( ) {
+            return this.saying;
+            };
+            var myCat = new Cat('Kitty');
+            var says = myCat.says( );
+            var name = myCat.get_name( );
+            
+            console.log(name);
+            console.log(says);
